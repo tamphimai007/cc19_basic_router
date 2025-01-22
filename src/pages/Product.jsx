d2003b2cbad0250acf8e032af19ed341f7a65f47
phongphat <phongphat.jap@gmail.com>
2025-01-22 14:32:07 +0700

    update useState

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,16 +1,30 @@
 // rfce
+import { useEffect } from "react";
 import useProductStore from "../store/product-store";
 function Product() {
   // Javascript
   const products = useProductStore((state) => state.products);
-  const actionGetData = useProductStore((state)=>state.actionGetData)
-  console.log(products);
-  return <div>
-    <button 
-    onClick={actionGetData}
-    className="bg-green-400">
+  const actionGetData = useProductStore((state) => state.actionGetData);
+  
+    useEffect(()=>{
+        // code body
+        actionGetData()
+    },[])
+
+  return (
+    <div>
+      <button onClick={actionGetData} className="bg-green-400">
         Get Data
-        </button>
-    </div>;
+      </button>
+      <hr />
+
+      {
+        products?.map((product)=>{
+            console.log(product)
+            return <li>{product.title}</li>
+        })
+      }
+    </div>
+  );
 }
 export default Product;
