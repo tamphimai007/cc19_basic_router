c97f318fca7c7a7cb70011736a5faccd211e1b9d
phongphat <phongphat.jap@gmail.com>
2025-01-22 10:48:05 +0700

    update

--- src/pages/RecapUseState.jsx
+++ src/pages/RecapUseState.jsx
@@ -1,26 +1,56 @@
-import { useState } from "react"
+import { useState } from "react";
 
 // rfce
 function RecapUseState() {
   // Javascript
-  const num = 6
-  const [number,setNumber] = useState(4)
-  console.log(number)
+  const num = 6;
+  const [number, setNumber] = useState(4);
+  const [number2, setNumber2] = useState({
+    num: 5,
+  });
+  console.log(number2.num);
 
-  const hdlIncrease = ()=>{
-    console.log('Easy, useState')
-  }
+  const hdlIncrease = () => {
+    setNumber((prev) => prev + 1);
+  };
+  const hdlDecrease = () => {
+    setNumber((prev) => prev - 1);
+  };
+  const hdlIecrease2 = () => {
+    setNumber2((prev) => ({ num: prev.num + 1 }));
+  };
 
   return (
     <div>
-        <h1 className="text-8xl">{number}</h1>
-        <br/>
-        <button 
+      <h1 className="text-8xl">{number}</h1>
+      <br />
+      <button
         onClick={hdlIncrease}
         className="bg-green-500 p-2 
         hover:scale-105 hover:duration-300 hover:bg-green-700
-        rounded-md shadow-md">Increase</button>
+        rounded-md shadow-md"
+      >
+        Increase
+      </button>
+      <button
+        onClick={hdlDecrease}
+        className="bg-green-500 p-2 
+        hover:scale-105 hover:duration-300 hover:bg-green-700
+        rounded-md shadow-md"
+      >
+        Decrease
+      </button>
+      <hr />
+      <p className="text-8xl">{number2.num}</p>
+      <button
+        onClick={hdlIecrease2}
+        className="bg-green-500 p-2 
+        hover:scale-105 hover:duration-300 hover:bg-green-700
+        rounded-md shadow-md"
+      >
+        Increase2
+      </button>
     </div>
-  )
+  );
 }
-export default RecapUseState
+export default RecapUseState;
