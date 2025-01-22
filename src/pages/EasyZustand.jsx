cccc1157a5bd1ae69eba55a6f01909b36b324b94
phongphat <phongphat.jap@gmail.com>
2025-01-22 13:22:26 +0700

    update

--- src/pages/EasyZustand.jsx
+++ src/pages/EasyZustand.jsx
@@ -4,12 +4,32 @@ import useTamStore from "../store/tamStore";
 function EasyZustand() {
   // javascript
 
+  // Step 2 Use Store
+  const firstName = useTamStore((state) => state.firstName);
+  const address = useTamStore((state) => state.address);
+  const profile = useTamStore((state) => state.profile);
+  const products = useTamStore((state) => state.products);
+  const number = useTamStore((state) => state.number);
+  const actionIncrease = useTamStore((state) => state.actionIncrease);
+  const actionDecrease = useTamStore((state) => state.actionDecrease);
+  console.log(number);
 
- // Step 2 Use Store
- const firstName = useTamStore((state)=>state.firstName)
- const address = useTamStore((state)=>state.address)
- console.log(address.street)
+  const hdlClick = () => {
+    // javascript .......
+    actionDecrease();
+  };
 
-  return <div>EasyZustand</div>;
+  return (
+    <div>
+      <p className="text-8xl">{number}</p>
+      <button onClick={actionIncrease} className="bg-green-400">
+        Increase
+      </button>
+      <br/>
+      <button onClick={hdlClick} className="bg-green-400">
+        Decrease
+      </button>
+    </div>
+  );
 }
 export default EasyZustand;
--- src/store/tamStore.jsx
+++ src/store/tamStore.jsx
@@ -1,9 +1,22 @@
 import { create } from "zustand";
 // Step 1 Create Store
-const tamStore = () => ({
+const tamStore = (set) => ({
   firstName: "phongphat",
   lastName: "japhichom",
   address: { street: "1234", zipcode: "567" },
+  profile: ["phongphat", "japhichom"],
+  products: [
+    { id: 1, title: "ASUS", price: 50000 },
+    { id: 2, title: "MSI", price: 40000 },
+  ],
+  number: 5,
+  actionIncrease: () => {
+    // * setNumber2((state) => ({ num: state.num + 1 }));
+    set((state) => ({ number: state.number + 1 }));
+  },
+  actionDecrease: () => {
+    set((state) => ({ number: state.number - 1 }));
+  },
 });
 const useTamStore = create(tamStore);
 export default useTamStore;
